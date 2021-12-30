6d36dcbb8f10dc0493b87af5a0d941ab600ad7ef
Timothy Langer <zeevox.dev@gmail.com>
2021-12-30 13:52:26 +0000

    Add discrete Fourier transform bar chart
    * Use library to calculate Fourier transform for integer frequency bins
    * Plot a bar graph with respective frequencies
    * Output stroke rate based on bar with highest magnitude
    * Because DFT is working on integer frequency bins only a resolution of
      15bpm is possible (TODO: improve resolution)
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- app/src/main/java/net/zeevox/nearow/MainActivity.kt
+++ app/src/main/java/net/zeevox/nearow/MainActivity.kt
@@ -6,27 +6,35 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.Legend.LegendForm
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.components.YAxis.AxisDependency
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.google.android.material.textview.MaterialTextView
 import net.zeevox.nearow.databinding.ActivityMainBinding
 import net.zeevox.nearow.input.DataCollectionService
+import se.imagick.ft.slidingdft.DFTSlider
+import se.imagick.ft.slidingdft.DFTSliderImpl
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-
     lateinit var chart: LineChart
+    lateinit var barChart: BarChart
+    lateinit var strokeRate: MaterialTextView
+
+    val MAX_STROKE_RATE = 100
+
+    val slider: DFTSlider = DFTSliderImpl(MAX_STROKE_RATE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Nearow)
@@ -35,12 +43,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.root.keepScreenOn = true
 
+        strokeRate = findViewById(R.id.stroke_rate)
+
         LocalBroadcastManager.getInstance(this).registerReceiver(
             broadcastReceiver, IntentFilter(
                 DataCollectionService.KEY_ON_SENSOR_CHANGED_ACTION
             )
         )
 
+        setupChart()
+        setupBarChart()
+    }
+
+    private fun setupChart() {
         chart = findViewById(R.id.live_chart)
 
         val data = LineData()
@@ -72,7 +87,13 @@ class MainActivity : AppCompatActivity() {
 
         val rightAxis: YAxis = chart.axisRight
         rightAxis.isEnabled = false
+    }
 
+    private fun setupBarChart() {
+        barChart = findViewById(R.id.bar_chart)
+        val data = BarData()
+        barChart.data = data
+        chart.description.isEnabled = false
     }
 
     override fun onResume() {
@@ -112,6 +133,19 @@ class MainActivity : AppCompatActivity() {
         return set
     }
 
+    private fun createFourierSet(values: List<BarEntry>) {
+        val set1: BarDataSet
+
+        if (barChart.data != null &&
+            barChart.data.dataSetCount > 0
+        ) {
+            set1 = barChart.data.getDataSetByIndex(0) as BarDataSet
+            set1.values = values
+            barChart.data.notifyDataChanged()
+            barChart.notifyDataSetChanged()
+        } else barChart.data = BarData(listOf(BarDataSet(values, "DFT")))
+    }
+
 
     private fun addEntry(value: Float) {
         val data: LineData = chart.data
@@ -144,6 +178,47 @@ class MainActivity : AppCompatActivity() {
                 intent.getFloatExtra(DataCollectionService.KEY_ACCELERATION_MAGNITUDE, -1.0f)
             binding.accelerationReading.text = acceleration.toString()
             addEntry(acceleration)
+
+            slider.slide(acceleration.toDouble())
+
+            val values = mutableListOf<BarEntry>()
+
+            var maxMagnitude: Double = -1.0
+            var maxFrequencyBin: Int = 0
+
+            for (frequencyComponentNo in 1..MAX_STROKE_RATE) {
+                Log.d(this.javaClass.simpleName, slider.latencyInSamples.toString())
+                val polar =
+                    slider.getPolar(frequencyComponentNo) // NB! See documentation about instance reuse.
+
+                val complex =
+                    slider.getComplex(frequencyComponentNo) // NB! See documentation about instance reuse.
+
+                val realSum = slider.getRealSum(false)
+
+                if (polar.magnitude > maxMagnitude) {
+                    maxMagnitude = polar.magnitude
+                    maxFrequencyBin = frequencyComponentNo
+                }
+
+                values.add(
+                    BarEntry(
+                        frequencyComponentNo.toFloat(),
+                        polar.magnitude.toFloat()
+                    )
+                )
+
+                Log.d(
+                    this.javaClass.simpleName,
+                    "$frequencyComponentNo ${polar.magnitude} ${polar.phase} ${complex.real} ${complex.imaginary} $realSum"
+                )
+            }
+
+            createFourierSet(values)
+            barChart.invalidate()
+
+            strokeRate.text = (maxFrequencyBin.toFloat() * 50 / (MAX_STROKE_RATE * 2) * 60).toString() + "bpm"
+
         }
     }
 }
